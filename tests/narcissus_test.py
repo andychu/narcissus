2d9f3d9cd93cb87a04c57eafb052ff25ef462a78
Andy Chu <andy@chubot.org>
2009-12-20 00:08:37 -0500

    Testing Python against JS

--- tests/narcissus_test.py
+++ tests/narcissus_test.py
@@ -18,7 +18,38 @@ from pan.test import testy
 
 PY_NARCISSUS_ROOT = '/home/andy/svn/pynarcissus-read-only'
 
+
+class JsVerifier(testy.StandardVerifier):
+  """Verify the JS implementation of Narcissus."""
+
+  def __init__(self, *args, **kwargs):
+    testy.StandardVerifier.__init__(self, *args, **kwargs)
+    self.js_runner = os_process.Runner()
+    self.py_runner = os_process.Runner(cwd=PY_NARCISSUS_ROOT)
+
+  def _TestPath(self, path):
+    return os.path.join(PY_NARCISSUS_ROOT, path)
+
+  def _TestContents(self, path):
+    return open(self._TestPath(path)).read()
+
+  def ParseTree(self, test_file):
+    js_input = self._TestContents(test_file)
+
+    cmd = './sexp.py'
+    p = self.py_runner.Pipes(cmd, 'IO')
+    py_out, _ = p.communicate(js_input)
+
+    print py_out
+
+    js_cmd = './nw.sh $PWD/bin/narcissus %s' % self._TestPath(test_file)
+    js_out = self.js_runner.Result(js_cmd).stdout
+    print js_out
+
+
 class SimpleTest(testy.Test):
+
+  VERIFIERS = [JsVerifier]
 
   def setUpOnce(self):
     self.runner = os_process.Runner(cwd=PY_NARCISSUS_ROOT)
@@ -27,14 +58,12 @@ class SimpleTest(testy.Test):
     cmd = './jsparser.py tests/mjsunit/source/regress-1039610.js'
     print self.runner.Result(cmd).stdout
 
-  def testSmallSexp(self):
-    cmd = './sexp.py'
-    js_input = open(os.path.join(
-        PY_NARCISSUS_ROOT, 'tests/mjsunit/source/regress-1039610.js')).read()
-    p = self.runner.Pipes(cmd, 'IO')
-    stdout, stderr = p.communicate(js_input)
-    print stdout
+  def testSmallExample(self):
+    self.verify.ParseTree('tests/mjsunit/source/regress-1039610.js')
 
 
 if __name__ == '__main__':
+  # One-time setup
+  os.system('build/makelib.py')
+
   testy.RunThisModule()
