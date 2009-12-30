258f3d2a8b597ae4a55a53ea2c151a121fe91914
Andy Chu <andy@chubot.org>
2009-12-30 03:07:24 -0500

    Allow passing a file to parse as the first argument

--- d8.sh
+++ d8.sh
@@ -4,10 +4,13 @@
 # Author: Andy Chu
 #
 # Demo of running narcissus directly under d8.
+# 
+# Usage: d8.sh <file to parse>
 
 time ~/svn/v8-read-only/d8 \
     -e 'var exports={};' \
     ../../engines/default/lib/json.js \
     ../../engines/default/lib/object.js \
     lib/narcissus.js \
-    -e 'print(JSON.stringify(parse("var i=0;"), null, 2));'
+    -e "var jsCode=read('$1');" \
+    -e 'print(JSON.stringify(parse(jsCode), null, 2));'
