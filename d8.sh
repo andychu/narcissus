3b15721875e59d5092f9773376fd2e87a63c6ac0
Andy Chu <andy@chubot.org>
2009-12-30 11:13:35 -0500

    Allow both parsing and executing

--- d8.sh
+++ d8.sh
@@ -7,10 +7,19 @@
 # 
 # Usage: d8.sh <file to parse>
 
+action=$1
+filename=$2
+
+if [ $action == "parse" ]; then
+  code='print(JSON.stringify(parse(jsCode), null, 2));'
+else
+  code='evaluate(jsCode);'
+fi
+
 time ~/svn/v8-read-only/d8 \
     -e 'var exports={};' \
     ../../engines/default/lib/json.js \
     ../../engines/default/lib/object.js \
     lib/narcissus.js \
-    -e "var jsCode=read('$1');" \
-    -e 'print(JSON.stringify(parse(jsCode), null, 2));'
+    -e "var jsCode=read('$filename');" \
+    -e $code
